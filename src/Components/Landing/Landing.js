c603d75b4f1e6b20a9a4f1debd7b6cc5d33765cd
Eric Sellors <github@esellors.com>
2020-10-04 23:28:24 -0500

    initial

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Landing.scss';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <h2>Welcome</h2>
+      <p>
+        Breaking Bad is an American neo-Western crime drama television series created and produced by Vince Gilligan.
+      </p>
+      <p>
+        The show aired on AMC from January 20, 2008 to September 29, 2013, consisting of five seasons for a total of 62 episodes. It was set and filmed in Albuquerque, New Mexico, and tells the story of Walter White (Bryan Cranston), an underemployed and depressed high school chemistry teacher who is struggling with a recent diagnosis of stage-three lung cancer.
+      </p>
+      <p>
+        White turns to a life of crime, partnering with his former student Jesse Pinkman (Aaron Paul), by producing and distributing crystallized methamphetamine to secure his family's financial future before he dies, while navigating the dangers of the criminal underworld.
+      </p>
+    </section>
+  )
+}
+
+export default Landing;
